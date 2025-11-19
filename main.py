cb8b30138c8f30c0f3f55315a84238f77fc36b57
halpe-hal <nishimura@kklia.com>
2025-11-19 10:38:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,50 @@
 import streamlit as st
 from modules import course_master, course_reservation, course_progress_view
 
+
+
 def main():
     st.set_page_config(page_title="コース進行管理システム", layout="wide")
 
-    st.title("コース進行管理システム")
+    st.markdown(
+    """
+    <style>
+    /* ページ全体の上部余白を縮める */
+    .block-container {
+        padding-top: 1rem !important;
+    }
+    /* タイトル周辺の余白も縮める */
+    header[data-testid="stHeader"] {
+        height: 0rem !important;
+        padding: 0 !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
     menu = st.sidebar.radio(
         "メニュー",
         [
-            "1. コースマスタ管理",
-            "2. コース予約登録",
-            "3. コース進行ボード",
-            "4. 調理済み一覧",
-            "5. 配膳済み一覧",
+            "1. コース進行ボード",
+            "2. 調理済み一覧",
+            "3. 配膳済み一覧",
+            "4. コース予約登録",
+            "5. コースマスタ管理",
         ]
     )
 
-    if menu == "1. コースマスタ管理":
-        course_master.show()
-    elif menu == "2. コース予約登録":
-        course_reservation.show()
-    elif menu == "3. コース進行ボード":
+    if menu == "1. コース進行ボード":
         course_progress_view.show_board()
-    elif menu == "4. 調理済み一覧":
+    elif menu == "2. 調理済み一覧":
         course_progress_view.show_cooked_list()
-    elif menu == "5. 配膳済み一覧":
+    elif menu == "3. 配膳済み一覧":
         course_progress_view.show_served_list()
+    elif menu == "4. コース予約登録":
+        course_reservation.show()
+    elif menu == "5. コースマスタ管理":
+        course_master.show()
+
 
 if __name__ == "__main__":
     main()
